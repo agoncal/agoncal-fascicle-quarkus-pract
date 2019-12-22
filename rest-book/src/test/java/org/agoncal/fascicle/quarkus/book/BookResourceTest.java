b909f503ac6dc440a0c0a5a4f398c2c46b690576
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-12-22 15:59:40 +0100

    Adding container tests

--- rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/BookResourceTest.java
+++ rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/BookResourceTest.java
@@ -13,6 +13,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.math.BigDecimal;
 
 import static io.restassured.RestAssured.given;
+import static javax.ws.rs.core.HttpHeaders.ACCEPT;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.OK;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
@@ -61,6 +64,54 @@ public class BookResourceTest {
       .statusCode(200)
       .body(is("ping"));
   }
+
+  // tag::adocOpenAPI[]
+  @Test
+  void shouldPingOpenAPI() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON)
+      .when().get("/openapi")
+      .then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingSwaggerUI() {
+    given()
+      .when().get("/swagger-ui")
+      .then()
+      .statusCode(OK.getStatusCode());
+  }
+  // end::adocOpenAPI[]
+
+  // tag::adocHealth[]
+  @Test
+  void shouldPingLiveness() {
+    given()
+      .when().get("/health/live")
+      .then()
+      .statusCode(OK.getStatusCode());
+  }
+
+  @Test
+  void shouldPingReadiness() {
+    given()
+      .when().get("/health/ready")
+      .then()
+      .statusCode(OK.getStatusCode());
+  }
+  // end::adocHealth[]
+
+  // tag::adocMetrics[]
+  @Test
+  void shouldPingMetrics() {
+    given()
+      .header(ACCEPT, APPLICATION_JSON)
+      .when().get("/metrics/application")
+      .then()
+      .statusCode(OK.getStatusCode());
+  }
+  // end::adocMetrics[]
 
   @Test
   public void shouldNotFindDummy() {
