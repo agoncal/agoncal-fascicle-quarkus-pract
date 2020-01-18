cecc95ebc59ba67f86dafdbc601afa0cbfb73ac5
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-01-18 15:36:21 +0100

    Adoc

--- rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/BookApplication.java
+++ rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/BookApplication.java
@@ -4,7 +4,6 @@ import org.eclipse.microprofile.openapi.annotations.ExternalDocumentation;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
-import org.eclipse.microprofile.openapi.annotations.servers.Server;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.ws.rs.ApplicationPath;
@@ -17,9 +16,6 @@ import javax.ws.rs.core.Application;
     description = "This API allows CRUD operations on books",
     version = "1.0",
     contact = @Contact(name = "@agoncal", url = "https://twitter.com/agoncal")),
-  servers = {
-    @Server(url = "http://localhost:8083")
-  },
   externalDocs = @ExternalDocumentation(url = "https://github.com/agoncal/agoncal-fascicle-quarkus-pract", description = "All the Practicing Quarkus code"),
   tags = {
     @Tag(name = "api", description = "Public API that can be used by anybody"),
