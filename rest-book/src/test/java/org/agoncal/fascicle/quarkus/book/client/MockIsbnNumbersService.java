040381a6ea9fac65a58a41cd40273eec2a0a4d4f
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-02-17 14:32:54 +0100

    Fallback

--- rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/client/MockIsbnNumbersService.java
+++ rest-book/src/test/java/org/agoncal/fascicle/quarkus/book/client/MockIsbnNumbersService.java
@@ -2,12 +2,10 @@
 package org.agoncal.fascicle.quarkus.book.client;
 
 import io.quarkus.test.Mock;
+import org.agoncal.fascicle.quarkus.book.BookResourceTest;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 import javax.enterprise.context.ApplicationScoped;
-
-import static org.agoncal.fascicle.quarkus.book.BookResourceTest.MOCK_ISBN_10;
-import static org.agoncal.fascicle.quarkus.book.BookResourceTest.MOCK_ISBN_13;
 
 // tag::adocSnippet[]
 @Mock
@@ -18,8 +16,8 @@ public class MockIsbnNumbersService implements IsbnNumbersService {
   @Override
   public IsbnNumbers generateIsbnNumbers() {
     IsbnNumbers isbnNumbers = new IsbnNumbers();
-    isbnNumbers.setIsbn13(MOCK_ISBN_13);
-    isbnNumbers.setIsbn10(MOCK_ISBN_10);
+    isbnNumbers.setIsbn13(BookResourceTest.MOCK_ISBN_13);
+    isbnNumbers.setIsbn10(BookResourceTest.MOCK_ISBN_10);
     return isbnNumbers;
   }
 }
