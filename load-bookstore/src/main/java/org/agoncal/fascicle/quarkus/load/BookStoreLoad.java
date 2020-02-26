25645c8f689bbf2907f64f2e92d198b2c1d450af
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-02-26 11:01:07 +0100

    Load

--- load-bookstore/src/main/java/org/agoncal/fascicle/quarkus/load/BookStoreLoad.java
+++ load-bookstore/src/main/java/org/agoncal/fascicle/quarkus/load/BookStoreLoad.java
@@ -1,11 +1,13 @@
 package org.agoncal.fascicle.quarkus.load;
 
+// tag::adocSnippet[]
 public class BookStoreLoad {
 
-    public static void main(String[] args) {
-        Thread bookScenario = new Thread(new ScenarioBook());
-        bookScenario.start();
-        Thread numberScenario = new Thread(new ScenarioNumber());
-        numberScenario.start();
-    }
+  public static void main(String[] args) {
+    Thread bookScenario = new Thread(new ScenarioBook());
+    bookScenario.start();
+    Thread numberScenario = new Thread(new ScenarioNumber());
+    numberScenario.start();
+  }
 }
+// end::adocSnippet[]
