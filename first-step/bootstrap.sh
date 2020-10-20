b4b6d944e3b772e67173675b59b19735c6271873
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-20 14:09:36 +0200

    bump quarkus

--- first-step/bootstrap.sh
+++ first-step/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-mvn io.quarkus:quarkus-maven-plugin:1.8.2.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.8.3.Final:create \
+    -DplatformVersion=1.8.3.Final \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=first-step \
     -DclassName="org.agoncal.fascicle.quarkus.firststep.AuthorResource" \
