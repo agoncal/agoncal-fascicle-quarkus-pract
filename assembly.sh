486018c57e7ab0dc40458a6b250551f905a2bf5e
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-20 16:49:42 +0200

    Assembly

--- assembly.sh
+++ assembly.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+cp pom.xml pom.xml.back
 mv pom_to_be_renamed.xml pom.xml
-mv rest-number/pom_to_be_renamed.xml rest-number/pom.xml
-mv rest-book/pom_to_be_renamed.xml rest-book/pom.xml
 
 mvn assembly:single
+
+mv pom.xml pom_to_be_renamed.xml
+cp pom.xml.back pom.xml
