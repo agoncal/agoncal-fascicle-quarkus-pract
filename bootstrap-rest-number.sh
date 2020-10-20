839d7f9951ccea7e440c3641b34ade6c5ad3820a
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-20 17:38:54 +0200

    Refactory path

--- bootstrap-rest-number.sh
+++ bootstrap-rest-number.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+# tag::adocBootstrap[]
 mvn io.quarkus:quarkus-maven-plugin:1.8.3.Final:create \
     -DplatformVersion=1.8.3.Final \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=rest-number \
     -DprojectVersion=1.1-SNAPSHOT \
-    -DclassName="org.agoncal.fascicle.quarkus.BookResource" \
-    -Dpath="/api/books" \
-    -Dextensions="resteasy-jsonb"
+    -DclassName="org.agoncal.fascicle.quarkus.number.NumberResource" \
+    -Dpath="/api/numbers" \
+    -Dextensions="resteasy, resteasy-jsonb, smallrye-openapi"
+# end::adocBootstrap[]
 
-cd rest-book
+cd rest-number
 
 ./mvnw quarkus:add-extension -Dextensions="hibernate-validator"
