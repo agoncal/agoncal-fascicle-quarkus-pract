58e8974d02bbdabddd68d3859d8aea2faca2b48d
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-23 11:47:30 +0200

    adoc

--- bootstrap-rest-book.sh
+++ bootstrap-rest-book.sh
@@ -5,9 +5,9 @@ mvn io.quarkus:quarkus-maven-plugin:1.9.0.Final:create \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=rest-book \
     -DprojectVersion=1.1-SNAPSHOT \
-    -DclassName="org.agoncal.fascicle.quarkus.BookResource" \
+    -DclassName="org.agoncal.fascicle.quarkus.book.BookResource" \
     -Dpath="/api/books" \
-    -Dextensions="resteasy-jsonb"
+    -Dextensions="resteasy, hibernate-orm-panache, jdbc-postgresql, hibernate-validator, smallrye-openapi"
 # end::adocBootstrap[]
 
 cd rest-book
