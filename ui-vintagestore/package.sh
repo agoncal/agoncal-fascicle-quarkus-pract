e6130e68ef3f3481e87b24ab758ea38085bd59ac
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-24 16:15:53 +0200

    fixing packaging

--- ui-vintagestore/package.sh
+++ ui-vintagestore/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # tag::adocSnippet[]
-export DEST=src/main/resources/META-INF/resources/ui-vintagestore
+export DEST=src/main/resources/META-INF/resources
 ./node_modules/.bin/ng build --prod --base-href "."
-rm -Rf ${DEST}
-mkdir -p ${DEST}
+rm -Rf ${DEST}/ui-vintagestore
+mkdir -p ${DEST}/ui-vintagestore
 cp -R dist/* ${DEST}
 # end::adocSnippet[]
 
