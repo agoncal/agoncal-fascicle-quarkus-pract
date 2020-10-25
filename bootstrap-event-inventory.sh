fa5951fa63a2be1b328a5a7acf31d5419b1269f2
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-25 21:41:24 +0100

    Inventory

---
+++ bootstrap-event-inventory.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# tag::adocBootstrap[]
+mvn io.quarkus:quarkus-maven-plugin:1.9.0.Final:create \
+    -DplatformVersion=1.9.0.Final \
+    -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
+    -DprojectArtifactId=event-inventory \
+    -DprojectVersion=1.1-SNAPSHOT \
+    -DclassName="org.agoncal.fascicle.quarkus.inventory.InventoryResource" \
+    -Dpath="/api/inventory" \
+    -Dextensions="resteasy, resteasy-jsonb, mongodb-panache, smallrye-openapi, kafka"
+# end::adocBootstrap[]
+
+cd event-inventory
+
+# tag::adocDependencyFaultTolerance[]
+./mvnw quarkus:add-extension -Dextensions="rest-client, smallrye-fault-tolerance"
+# end::adocDependencyFaultTolerance[]
