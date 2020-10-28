9c57b359dd51c67b081a370a780359dcb990f282
Antonio Goncalves <antonio.goncalves@gmail.com>
2020-10-28 15:58:44 +0100

    Adoc

--- rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/client/NumberProxy.java
+++ rest-book/src/main/java/org/agoncal/fascicle/quarkus/book/client/NumberProxy.java
@@ -1,11 +1,13 @@
 package org.agoncal.fascicle.quarkus.book.client;
 
+// tag::adocImports[]
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+// end::adocImports[]
 
 // tag::adocSnippet[]
 @Path("/api/numbers")
