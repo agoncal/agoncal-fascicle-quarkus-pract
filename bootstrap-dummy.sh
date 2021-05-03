4db9c2254400afc867acce43156eb78aba8a6971
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-05-03 22:01:33 +0200

    Bump Quarkus version

--- bootstrap-dummy.sh
+++ bootstrap-dummy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # tag::adocBootstrap[]
-mvn io.quarkus:quarkus-maven-plugin:1.12.2.Final:create \
-    -DplatformVersion=1.12.2.Final \
+mvn io.quarkus:quarkus-maven-plugin:1.13.2.Final:create \
+    -DplatformVersion=1.13.2.Final \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=dummy
 # end::adocBootstrap[]
