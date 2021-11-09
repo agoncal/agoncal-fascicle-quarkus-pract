812e4e6b1c7e78b16456553d168acd129fb85899
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-11-09 04:36:49 +0100

    update bootstrap scripts

--- bootstrap-dummy.sh
+++ bootstrap-dummy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # tag::adocBootstrap[]
-mvn io.quarkus:quarkus-maven-plugin:2.0.0.Final:create \
-    -DplatformVersion=2.0.0.Final \
+mvn io.quarkus:quarkus-maven-plugin:2.4.1.Final:create \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=dummy
 # end::adocBootstrap[]
