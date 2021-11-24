a21f7ba83885038fe5ea27c5615d3a339ca2f4a4
Antonio Goncalves <antonio.goncalves@gmail.com>
2021-11-24 20:14:45 +0100

    Bump 2.5.0

--- bootstrap-rest-book.sh
+++ bootstrap-rest-book.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # tag::adocBootstrap[]
-mvn -U io.quarkus:quarkus-maven-plugin:2.4.1.Final:create \
+mvn -U io.quarkus:quarkus-maven-plugin:2.5.0.Final:create \
+    -DplatformVersion=2.5.0.Final \
     -DprojectGroupId=org.agoncal.fascicle.quarkus-practising \
     -DprojectArtifactId=rest-book \
     -DprojectVersion=2.0.0-SNAPSHOT \
