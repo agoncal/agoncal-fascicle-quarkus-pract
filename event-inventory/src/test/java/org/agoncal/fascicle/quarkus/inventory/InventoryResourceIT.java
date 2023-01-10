4e50211d3ed6416f35f47e0e91c15969231ce574
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-01-10 06:36:21 +0100

    eventory

---
+++ event-inventory/src/test/java/org/agoncal/fascicle/quarkus/inventory/InventoryResourceIT.java
@@ -0,0 +1,8 @@
+package org.agoncal.fascicle.quarkus.inventory;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class InventoryResourceIT extends InventoryResourceTest {
+    // Execute the same tests but in packaged mode.
+}
--- event-inventory/src/test/java/org/agoncal/fascicle/quarkus/inventory/NativeInventoryResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.agoncal.fascicle.quarkus.inventory;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeInventoryResourceIT extends InventoryResourceTest {
-
-    // Execute the same tests but in native mode.
-}
